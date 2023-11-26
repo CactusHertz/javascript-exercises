ea0ccd80cc38553d3f430a8577fad4d70077f024
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2023-11-25 16:04:49 -0800

    Complete 03_reverseString excercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let finalString = '';
+    for (let i = string.length; i > 0; i--){
+        finalString = finalString + string.charAt(i - 1);
+    }
+    return finalString
 };
 
 // Do not edit below this line
