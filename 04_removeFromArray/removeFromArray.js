67f6babebbdc7405f6b9ba78cf1caea16124d587
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2023-11-26 16:21:01 -0800

    Complete 04_removeFromArray excercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(list) {
+    const args = Array.prototype.slice.call(arguments, 1);
 
+
+
+    for (let i = 0; i < args.length; i++){
+        let index = list.indexOf(args[i]);
+
+        if (index >= 0 ){
+            list.splice(index, 1);
+        }
+    }
+  
+    return list;
 };
 
 // Do not edit below this line
