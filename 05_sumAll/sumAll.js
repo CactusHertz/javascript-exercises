8888fad6f0f312f859b4ff747989a68661bd3661
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2023-11-26 17:14:17 -0800

    Complete 05_sumAll excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let finalSum = 0;
+    let min = 0;
+    let max = 0;
+    if (typeof start !== 'number' || typeof end !== 'number'){
+        return 'ERROR';
+    }
+    if (start < 0 || end < 0){
+        return 'ERROR';
+    }
+    
 
+    if (start < end){
+        min = start;
+        max = end;
+    }
+    else{
+        min = end;
+        max = start;
+    }
+
+    for (let i = min; i < max + 1; i++){
+        finalSum += i;
+    }
+    
+    return finalSum;
 };
 
 // Do not edit below this line
