dc0230ed566698e0659ecf2a9a30227c32759e38
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2023-12-10 01:20:10 -0800

    Complete 08_calculator excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+	return list.reduce((total, num) => total + num , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+  return list.reduce((total, num) => total * num);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1){
+    return 1;
+  }
+  let total = 1;
+  for (let i = 2; i < num + 1; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
