d09811598d038f32c23a9b486335fbe7467f7bf1
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2023-12-10 11:27:21 -0800

    Complete 09_palindromes excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanStr = str.replace(/[^a-zA-Z0-9]+/g, "").toLowerCase();
+    let reverseStr = '';
+    for(let i = cleanStr.length - 1; i > -1; i--){
+        reverseStr = reverseStr + cleanStr[i];
+    }
+    return cleanStr === reverseStr;
 };
 
 // Do not edit below this line
