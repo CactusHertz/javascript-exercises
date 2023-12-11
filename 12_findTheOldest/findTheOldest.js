e69666b9821696cc39dc1ac6ab4820b831bc1792
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2023-12-10 23:23:47 -0800

    Complete 12_findTheOldest excercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ordered = people.sort(function(a,b) {
+        const currentYear = (new Date()).getFullYear();
+        let lastGuy = 0;
+        if ('yearOfDeath' in a){
+            lastGuy = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            lastGuy = currentYear - a.yearOfBirth;
+        }
+        let nextGuy = 0;
+        if('yearOfDeath' in b){
+            nextGuy = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            nextGuy = currentYear - b.yearOfBirth;
+        }
+        return lastGuy > nextGuy ? -1:1;
+    });
+    return ordered[0];
 };
 
 // Do not edit below this line
